3b595bb21202253c5128811a181c973596100c7c
Ahmed <elzek989@gmail.com>
2026-03-09 12:20:15 +0200

    Add customized list task item

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:todo_nti5/core/customized_widgets/customized_list_task_item.dart';
 import 'package:todo_nti5/core/customized_widgets/empty_tasks_prompt.dart';
 import 'package:todo_nti5/core/customized_widgets/header_profile.dart';
 import 'package:todo_nti5/core/resources/app_assets.dart';
@@ -17,7 +18,10 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Column(
-          children: [HeaderProfile(), Expanded(child: EmptyTasksPrompt())],
+          children: [
+            HeaderProfile(),
+            Expanded(child: EmptyTasksPrompt()),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
@@ -30,7 +34,7 @@ class HomeScreen extends StatelessWidget {
               AppColors.transparentGreen,
               BlendMode.srcIn,
             ),
-          )
+          ),
         ),
       ),
     );
